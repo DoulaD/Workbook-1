2040932d6ea63c94c49e9c9c837d089c3ec3f562
DoulaD <Douladoumbia@gmail.com>
2025-04-10 10:42:47 -0400

    complete shoppinglist app

---
+++ first-maven/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Milk, cheese, eggs, honey, turkey, salad, onions, carrots, cereal");
+    }
+
+
+}
